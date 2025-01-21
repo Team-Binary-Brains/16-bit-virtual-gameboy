0eddc3b11c2259e2d80c9156d68fd22ccce35724
Soham Metha <sohammetha01@gmail.com>
2025-01-21 21:04:12 +0530

    Naming + File organisation
    Did a proper structuring of the previously merged branch with updated names as discussed in offline meet. Also moved sample programs out of the project folder.

--- O#/build.sh
+++
@@ -1,1 +0,0 @@
-gcc main.c src/O#_lexer.c src/O#_parser.c src/O#_codegenerator.c -o build/occ
--- O#/headers/O#_codegenerator.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-int generateCode(Node *root, char *filename);
-void traverseTree(Node *node, int is_left, FILE *file);
